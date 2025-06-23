eeca94a205689b1990981121027f6fc4a9cd9f1b
s1kko1337 <r.melo.kk@gmail.com>
2025-06-23 17:19:08 +0300

    Add spatie/sanctum to project, make base migtrations

---
+++ database/migrations/2025_06_23_134256_create_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignId('trainer_id')->constrained('users','id');
+            $table->foreignId('gym_id')->constrained('gyms','id');
+            $table->foreignId('activity_type_id')->constrained('activity_types','id');
+            $table->unsignedTinyInteger('max_clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activites');
+    }
+};
